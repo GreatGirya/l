12cd61cb8aefcde13731164378ad0fd1a88b15bd
Girya <kukumbos05@gmail.com>
2021-06-04 17:21:08 +1000

    Initial commit

---
+++ loz1.java
@@ -0,0 +1,18 @@
+public class loz2 {
+  public static void main(String[] args) {
+
+    int a = 6;
+    int b = 6;
+    if (a > b) {
+      System.out.println(a);
+    }
+    if (a < b) {
+      System.out.println(b);
+    }
+    if (a == b) {
+      System.out.println("DRAW");
+    }
+
+  }
+}
+
